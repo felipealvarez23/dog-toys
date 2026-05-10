084179ffbacca8b05aef83fbc983be6d2e96d2f4
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com.co>
2026-05-10 17:53:15 -0500

    feat(greeting): add base backend app

---
+++ greeting/src/main/java/com/edalvarez/greeting/controllers/GreetingController.java
@@ -0,0 +1,12 @@
+package com.edalvarez.greeting.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
